43e15fc44a49e7ac0f79a3dbad684aef8132e872
BrandonKlotz <brandonklotz@icloud.com>
2021-02-11 18:31:19 -0500

    Update example to typescript and add prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,50 @@
 import * as React from "react";
-import Image from 'next/image';
+import Image from "next/image";
 
 type ImageTraceProps = {
-  src: string,
-  trace: string,
-  delay?: string,
-  width?: string,
-  height?: string,
-  nextImageProps?: any
-}
+  src: string;
+  trace: string;
+  delay?: string;
+  width?: string;
+  height?: string;
+  nextImageProps?: any;
+};
 
-function ImageTrace({ src, trace, delay='0.5s', width='auto', height='auto', nextImageProps }: ImageTraceProps) {
+function ImageTrace({
+  src,
+  trace,
+  delay = "0.5s",
+  width = "auto",
+  height = "auto",
+  nextImageProps,
+}: ImageTraceProps) {
   const [imageLoaded, setImageLoaded] = React.useState(false);
   const defaultStyles = {
     width: width,
     height: height,
-    transition: `opacity ${delay} ease`
+    transition: `opacity ${delay} ease`,
   };
 
   return (
-    <div style={{ position: 'relative', left: 0, top: 0 }}>
-      <div style={{ ...defaultStyles, ...{ position: 'absolute', opacity: imageLoaded ? 1 : 0 }}}>
-        <Image src={src} onLoad={() => setImageLoaded(true)} {...nextImageProps} />
+    <div style={{ position: "relative", left: 0, top: 0 }}>
+      <div
+        style={{
+          ...defaultStyles,
+          ...{ position: "absolute", opacity: imageLoaded ? 1 : 0 },
+        }}
+      >
+        <Image
+          src={src}
+          onLoad={() => setImageLoaded(true)}
+          {...nextImageProps}
+        />
       </div>
-      <div style={{...defaultStyles, ...{ opacity: imageLoaded ? 0 : 1, pointerEvents: 'none' }}}>
+      <div
+        style={{
+          ...defaultStyles,
+          ...{ opacity: imageLoaded ? 0 : 1, pointerEvents: "none" },
+        }}
+      >
         <Image src={trace} {...nextImageProps} />
       </div>
     </div>
